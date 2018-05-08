ee1464437e166024e78a473422ab6ca5cceb8979
tdolk <teo.dolci@outlook.com>
2018-05-08 11:35:08 +0200

    Website base
    Code de base, phrases random

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// array = liste d'éléments //
+var cards = [
+	"Demain",
+	"Sirop de framboise",
+	"La réponse se trouve au plafond",
+	"Inspire toi de la météo",
+];
+
+console.log("Nos cartes:"+cards);
+
+function	tirerCarte() {
+	var randomNumber
+};
