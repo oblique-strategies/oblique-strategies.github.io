247bcbd80b5b3fd5af7df87adfd476adfad382c0
Manuel Schmalstieg <git.code@ms-studio.net>
2018-05-23 07:14:05 +0200

    Déplacement du Javascript dans fichier
    On déplace tout le Javascript dans le fichier javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,59 @@
-// array = liste d'éléments //
+var startButton = document.getElementById('start');
+
+startButton.addEventListener('click', premiereCarte, false);
+
+function premiereCarte() {
+
+	var myCanvas = document.getElementById('myCanvas');
+	var intro    = document.getElementById('intro');
+	var frame    = document.getElementById('frame');
+
+	// Tirer une première carte
+
+	tirerCarte();
+
+	// Rendre transparente l'animation
+					
+	myCanvas.className += " transparent";
+
+	// Ce qui suit va se produire après un délai de 1000 millisecondes.
+	
+	setTimeout(function() {
+	
+		// Faire disparaître l'animation
+
+		intro.style.left = "100vw";
+
+		// Faire apparaître le contenu
+
+		frame.style.right = "0vw";
+
+	}, 1000);
+
+	event.preventDefault();
+
+} // fin de premiereCarte()
+
+
+//array = liste d'éléments
 var cards = [
 	"Demain",
 	"Sirop de framboise",
 	"La réponse se trouve au plafond",
 	"Inspire toi de la météo",
+	"Noir et blanc"
 ];
 
-console.log("Nos cartes:"+cards);
+function getRandom(min , max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-function	tirerCarte() {
-	var randomNumber
-};
+function tirerCarte() {
+	
+	// Déterminer le nombre de phrases
+	var cardNumber = cards.length - 1;
+
+	// Choix de la phrase
+	var randomNumber = getRandom(0,cardNumber);
+	document.getElementById('phrase').innerHTML = cards[randomNumber];
+}
