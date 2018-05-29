2ac8c166222287b171e4d711865e97d2ad43177c
mdutschler <31759610+mdutschler@users.noreply.github.com>
2018-05-29 10:42:03 +0200

    Ajout phrases + img MALANE

--- javascript/cards.js
+++ javascript/cards.js
@@ -7,11 +7,36 @@ var cards = [
 	  ["nomdufichier.jpg","Noir et blanc"],
 
 //Groupe MaLaNe
-	  ["nomdufichier.jpg","Demain"],
-	  ["nomdufichier.jpg","Sirop de framboise"],
-	  ["nomdufichier.jpg","La réponse se trouve au plafond"],
-	  ["nomdufichier.jpg","Inspire toi de la météo"],
-	  ["nomdufichier.jpg","Noir et blanc"],
+	  ["cartes-illustrations_Page_01.jpg","Un café le matin réveille les mouettes"],
+	  ["cartes-illustrations_Page_02.jpg","Ce qui fonctionne en noir blanc fonctionne en couleur"],
+	  ["cartes-illustrations_Page_03.jpg","Imprime ton travail"],
+		["cartes-illustrations_Page_04.jpg","Écoute de la musique classique"],
+		["cartes-illustrations_Page_05.jpg","Utilise le premier motif que tu vois"],
+		["cartes-illustrations_Page_06.jpg","Fais un acte soudain, destructif et imprévisible, incorpore"],
+		["cartes-illustrations_Page_07.jpg","Ferme les yeux"],
+		["cartes-illustrations_Page_08.jpg","Fais l’opposé de ce que tu as fait jusque-là"],
+		["cartes-illustrations_Page_09.jpg","Lève-toi et change de place"],
+		["cartes-illustrations_Page_10.jpg","Fais un brainstorming"],
+		["cartes-illustrations_Page_11.jpg","Va dans le minimalisme"],
+	  ["cartes-illustrations_Page_12.jpg","Sois plus agressif"],
+	  ["cartes-illustrations_Page_13.jpg","Écoute ta musique préférée"],
+		["cartes-illustrations_Page_14.jpg","Fais sortir les éléments du cadre"],
+		["cartes-illustrations_Page_15.jpg","Inspire-toi du Bauhaus"],
+		["cartes-illustrations_Page_16.jpg","Inspire-toi de l’art préhistorique"],
+		["cartes-illustrations_Page_17.jpg","Inspire-toi du Constructivisme"],
+		["cartes-illustrations_Page_18.jpg","Inspire-toi du Memphis style"],
+		["cartes-illustrations_Page_19.jpg","Inspire-toi du Surréalisme"],
+		["cartes-illustrations_Page_20.jpg","Écoute de l’ASMR"],
+		["cartes-illustrations_Page_21.jpg","Saute 10x sur place"],
+		["cartes-illustrations_Page_22.jpg","Écoute une chanson Walt Disney en boucle"],
+		["cartes-illustrations_Page_23.jpg","Va sur google Earth"],
+		["cartes-illustrations_Page_24.jpg","Fais 5 croquis"],
+		["cartes-illustrations_Page_25.jpg","Utilise la transparence"],
+		["cartes-illustrations_Page_26.jpg","Scotche-toi la face"],
+		["cartes-illustrations_Page_27.jpg","Prends de la distance pour évaluer ton travail"],
+		["cartes-illustrations_Page_28.jpg","Imprime toutes tes variantes"],
+		["cartes-illustrations_Page_29.jpg","Nettoie ton écran"],
+
 
 //Groupe NaMi
 	  ["nomdufichier.jpg","Demain"],
@@ -27,7 +52,7 @@ var cards = [
 	  ["nomdufichier.jpg","Inspire toi de la météo"],
 	  ["nomdufichier.jpg","Noir et blanc"],
 
-//Groupe SoMa 
+//Groupe SoMa
 	  ["nomdufichier.jpg","Demain"],
 	  ["nomdufichier.jpg","Sirop de framboise"],
 	  ["nomdufichier.jpg","La réponse se trouve au plafond"],
