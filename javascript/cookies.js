17cb9781b49ad3ff92e84f8c08ae2f99728778fb
Manuel Schmalstieg <git.code@ms-studio.net>
2018-06-12 21:04:27 +0200

    fonctions utilitaires pour gérer les cookies

---
+++ javascript/cookies.js
@@ -0,0 +1,33 @@
+// https://stackoverflow.com/questions/14573223/
+
+function setCookie(name,value,hours) {
+    var expires = "";
+    if (hours) {
+        var date = new Date();
+        date.setTime(date.getTime() + (hours*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+eraseCookie('heure');
+
+// setCookie('ppkcookie','testcookie',7);
+
+// var x = getCookie('ppkcookie');
+// if (x) {
+//     console.log('cookie: '+x);
+// }
